317cf3c719fdaef1b68b7661ad70ec2433f388f0
dwjhaines <David.Haines@s-a-m.com>
2018-03-29 15:47:06 +0100

    More updates to diary 2018

---
+++ js/diarytiles.js
@@ -0,0 +1,30 @@
+var app = angular.module('diaryApp',[]);
+app.controller('tileController', function() {
+    var weeks=[
+        {
+            link:'week-01.html',
+            image:'images/russia.JPG',
+            title:'Week 1',
+            category: ['diary']
+         },
+         {
+            link:'week-02.html',
+            image:'images/turtle.JPG',
+            title:'Week 2',
+            category: ['diary']
+         },
+         {
+            link:'week-03.html',
+            image:'images/week03.JPG',
+            title:'Week 3',
+            category: ['diary']
+         },
+         {
+            link:'week-04.html',
+            image:'images/manta.JPG',
+            title:'Week 4',
+            category: ['diary']
+         }
+    ]
+    this.weeks=weeks;
+});
