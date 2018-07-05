b4bf5b8471338ef3c8c305965838024b4992318b
dwjhaines <dwjhaines@gmail.com>
2018-07-05 13:44:21 +0300

    July 4th update

--- world_trip_2018/js/diarytiles.js
+++ world_trip_2018/js/diarytiles.js
@@ -38,6 +38,12 @@ app.controller('tileController', function() {
             category: ['diary']
         },
         {
+            link:'week-07.html',
+            image:'images/tiles/week07-tile.JPG',
+            title:'Week 7',
+            category: ['diary']
+        },
+        {
             link:'https://photos.app.goo.gl/Vn1VyjrhFdkJ97sE9',
             image:'images/tiles/week02-tile.JPG',
             title:'1. Europe',
