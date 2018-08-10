6498a09d1637c34f04e9e583408dd238458e2348
dwjhaines <dwjhaines@gmail.com>
2018-08-10 12:14:33 +0300

    August 9th update

--- world_trip_2018/js/diarytiles.js
+++ world_trip_2018/js/diarytiles.js
@@ -2,6 +2,12 @@ var app = angular.module('diaryApp',[]);
 app.controller('tileController', function() {
     var weeks=[
         {
+            link:'week-12.html',
+            image:'images/tiles/week12-tile.JPG',
+            title:'Week 12',
+            category: ['diary']
+        },
+        {
             link:'week-11.html',
             image:'images/tiles/week11-tile.JPG',
             title:'Week 11',
