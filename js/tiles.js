358db613d634d381f3786c25033377f517d7237d
dwjhaines <dwjhaines@gmail.com>
2018-12-11 16:06:43 +0800

    10th December update

--- js/tiles.js
+++ js/tiles.js
@@ -3,7 +3,7 @@ app.controller('tileController', function() {
     var trips=[
         {
             link:'world_trip_2018/index-2018.html',
-            image:'images/world-trip2018-tile.JPG',
+            image:'world_trip_2018/images/tiles/week27-tile.JPG',
             title:'World Trip 2018',
             category: ['travel']
          },
