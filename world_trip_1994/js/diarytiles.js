aac8da6fcc9376e6e9600c0eb5d2fc5552cf89d1
dwjhaines <dwjhaines@gmail.com>
2020-04-08 21:13:18 +0100

    World Trip 1994 week 2 update

--- world_trip_1994/js/diarytiles.js
+++ world_trip_1994/js/diarytiles.js
@@ -1,20 +1,18 @@
 var app = angular.module('diaryApp',[]);
 app.controller('tileController', function() {
     var weeks=[
-        
-        {
-            link:'week-02.html',
-            image:'images/tiles/week02-tile.JPG',
-            title:'Week 2',
-            category: ['diary']
-        },
         {
             link:'week-01.html',
             image:'images/tiles/week01-tile.JPG',
             title:'Week 1',
             category: ['diary']
+        },
+        {
+            link:'week-02.html',
+            image:'images/tiles/week02-tile.JPG',
+            title:'Week 2',
+            category: ['diary']
         }
-
     ]
     this.weeks=weeks;
 });
