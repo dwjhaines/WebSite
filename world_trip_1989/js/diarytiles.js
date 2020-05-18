3585022fd4d0f265932dcd9d191495998fd19154
dwjhaines <dwjhaines@gmail.com>
2020-05-18 21:26:22 +0100

    On to New Zealand

--- world_trip_1989/js/diarytiles.js
+++ world_trip_1989/js/diarytiles.js
@@ -6,6 +6,18 @@ app.controller('tileController', function() {
             image:'images/tiles/part01-tile.jpg',
             title:'Part 1 - U.S.A.',
             category: ['diary']
+        },
+        {
+            link:'part-02.html',
+            image:'images/tiles/part02-tile.jpg',
+            title:'Part 2 - Hawaii',
+            category: ['diary']
+        },
+        {
+            link:'part-03.html',
+            image:'images/tiles/part03-tile.jpg',
+            title:'Part 3 - New Zealand',
+            category: ['diary']
         }
     ]
     this.weeks=weeks;
