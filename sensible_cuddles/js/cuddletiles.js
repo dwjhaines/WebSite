f85a3c7519b6206936c13dff28e5d9635d9faa2c
dwjhaines <dwjhaines@gmail.com>
2020-11-12 09:26:05 +0000

    Sensible cuddle list added

---
+++ sensible_cuddles/js/cuddletiles.js
@@ -0,0 +1,149 @@
+var app = angular.module('diaryApp',[]);
+app.controller('tileController', function() {
+    var weeks=[
+        {
+            image:'images/pout.jpg',
+            title:'5/11 - Pout',
+            category: ['cuddle']
+        },
+        {
+            image:'images/seb.jpg',
+            title:'6/11 - Seb',
+            category: ['cuddle']
+        },
+        {
+            image:'images/moo.jpg',
+            title:'7/11 - Moo',
+            category: ['cuddle']
+        },
+        {
+            image:'images/wally.jpg',
+            title:'8/11 - Wally',
+            category: ['cuddle']
+        },
+        {
+            image:'images/thomas.jpg',
+            title:'9/11 - Thomas',
+            category: ['cuddle']
+        },
+        {
+            image:'images/percy.jpg',
+            title:'10/11 - Percy',
+            category: ['cuddle']
+        },
+        {
+            image:'images/hoggy.jpg',
+            title:'11/11 - Hoggy',
+            category: ['cuddle']
+        },
+        {
+            image:'images/bunnywunny.jpg',
+            title:'12/11 - Bunnywunny',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'13/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'14/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'15/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'16/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'17/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'18/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'19/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'20/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'21/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'22/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'23/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'24/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'25/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'26/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'27/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'28/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'29/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'30/11 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'1/12 - ',
+            category: ['cuddle']
+        },
+        {
+            image:'images/question-mark.jpg',
+            title:'2/12 - ',
+            category: ['cuddle']
+        }
+        
+
+    ]
+    this.weeks=weeks;
+});
+
