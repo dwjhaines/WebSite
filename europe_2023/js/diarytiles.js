35b146f7d35ccf4649c6302662989666626b041d
dwjhaines <dwjhaines@gmail.com>
2023-06-11 22:12:20 +0200

    Europe 2023 update

--- europe_2023/js/diarytiles.js
+++ europe_2023/js/diarytiles.js
@@ -3,13 +3,13 @@ app.controller('tileController', function() {
     var weeks=[
         {
             link:'week-01.html',
-            image:'images/tiles/week01-tile.JPG',
+            image:'images/tiles/week01-tile.jpg',
             title:'Week 1',
             category: ['diary']
         },
         {
             link:'week-02.html',
-            image:'images/tiles/week02-tile.JPG',
+            image:'images/tiles/week02-tile.jpg',
             title:'Week 2',
             category: ['diary']
         }
