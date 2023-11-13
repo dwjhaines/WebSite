82ea6f8cd133df656efca10d70d74673ed2e43da
dwjhaines <dwjhaines@gmail.com>
2023-11-13 12:20:04 +0000

    2009 updates

--- js/tiles.js
+++ js/tiles.js
@@ -698,6 +698,24 @@ app.controller('tileController', function() {
             category: ['beer']
         },
         {
+            link:'https://photos.app.goo.gl/oy5JpvNDBSRBmjE17',
+            image:'images/amsterdam-2009-tile.JPG',
+            title:'Amsterdam 2009',
+            category: ['football']
+        },   
+        {
+            link:'https://photos.app.goo.gl/2H75h1roHC9FAEbF6',
+            image:'images/bonaire-2009-tile.JPG',
+            title:'Bonaire 2009',
+            category: ['diving']
+        },   
+        {
+            link:'https://photos.app.goo.gl/sPXmKCCou3rxNAvK8',
+            image:'images/spain-2009-tile.JPG',
+            title:'Spain 2009',
+            category: ['football', 'travel']
+        },
+        {
             link:'https://photos.app.goo.gl/EC8VQ4bJc9W14mjq5',
             image:'images/2008-tile.jpg',
             title:'2008',
