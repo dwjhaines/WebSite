bad6c19b57962619444ebd4f96e4b15cb32d9853
David Haines <dwjhaines@gmail.com>
2025-03-25 16:35:56 +0000

    Unesco list added

---
+++ js/unesco-tiles.js
@@ -0,0 +1,193 @@
+var app = angular.module('tripApp',[]);
+app.controller('tileController', function() {
+    var trips=[ 
+        {
+            link:'',
+            image:'',
+            title:'Bath',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Blaenavon',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Blenheim Palace',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Canterbury Cathedral',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Castles of King Edward',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Cornwall Mining',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Derwent Valley Mills',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Dorset Jurassic Coast',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Durham Cathedral',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Edinburgh',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'English Lake District',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Flow Country - Peat Bogs',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Forth Bridge',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Fountains Abbey',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Frontiers - Antonine Wall',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Frontiers - Hadrians Wall',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Giants Causeway',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Gracehill',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Greenwich',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Ironbridge Gauge',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Jodrell Bank',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Kew Gardens',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Neolithic Orkney (Skara Brae)',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'New Lanark Mill',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Pontcysyllte Aquaduct',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Saltaire',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Slate Landscape',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'St Kilda',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Stonehenge (including Avebury)',
+            category: ['unesco']
+        },
+				{
+            link:'',
+            image:'',
+            title:'Tower of London',
+            category: ['unesco']
+        },
+				{
+            link:'https://photos.app.goo.gl/WrfTCRPnbsWSyM1HA',
+            image:'images/unesco/westminster-abbey-tile.jpg',
+            title:'Westminster Abbey - 24/3/25',
+            category: ['unesco']
+        }
+        
+    ]
+    this.trips=trips;
+});
